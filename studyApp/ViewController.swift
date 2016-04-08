78dd76a8abe0db9f882a8cc17735bc9d523f8224
cloud <cloud@choehounui-macbook-pro.local>
2016-04-09 07:56:25 +0900

    Initial Commit

---
+++ studyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  studyApp
+//
+//  Created by cloud on 2016. 4. 9..
+//  Copyright © 2016년 studyApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
