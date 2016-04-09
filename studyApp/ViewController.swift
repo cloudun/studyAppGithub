05d04c93d16a5d01937130b6efea776f30edb60e
cloud <cloud@choehounui-macbook-pro.local>
2016-04-09 20:06:43 +0900

    커밋

--- studyApp/ViewController.swift
+++ studyApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 
+        // f
         // Do any additional setup after loading the view, typically from a nib.
     }
 
